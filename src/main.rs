352ed845c4a1852b46a9a4fc5538311d812924c8
Eduard <edp475@student.bham.ac.uk>
2025-07-27 22:13:56 +0100

    added the ping pong ball, winning and have finished the game

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,57 @@
-use macroquad::math::Rect;
+use macroquad::math::{Circle, Rect};
 use macroquad::prelude::*;
 #[macroquad::main("MyGame")]
 async fn main() {
     let lbound = 100.0;
     let rbound = 600.0;
     let top = 100.0;
-    let bottom = 600.0;
-    let mut lrect = Rect::new(lbound, top, 15.0, 100.0);
-    let mut rrect = Rect::new(rbound, top, 15.0, 100.0);
-
-    loop {
+    let bottom = 400.0;
+    let mut lrect = Rect::new(lbound, top, 1.0, 100.0);
+    let mut rrect = Rect::new(rbound, top, 1.0, 100.0);
+    let mut ball = Circle::new((lbound + rbound) / 2.0, top, 12.0);
+    let mut direction = (1.0, 1.0);
+    let mut ball_speed = 1.0;
+    let out = loop {
         clear_background(BLACK);
-
-        if is_key_down(KeyCode::W) && lrect.h > top {
-            lrect.h -= 4.0;
+        ball.x -= direction.0 * ball_speed;
+        ball.y -= direction.1 * ball_speed;
+        ball_speed += 0.002;
+        if ball.y < top {
+            direction.1 = -1.0;
         }
-        if is_key_down(KeyCode::S) && lrect.h < bottom {
-            lrect.h += 4.0;
+        if ball.y > bottom {
+            direction.1 = 1.0;
         }
-        if is_key_down(KeyCode::Up) && rrect.h > top {
-            rrect.h -= 4.0;
+        if ball.x < lbound {
+            break "right wins";
         }
-        if is_key_down(KeyCode::Down) && rrect.h < bottom {
-            rrect.h += 4.0;
+        if ball.x > rbound {
+            break "left wins";
         }
-        draw_line(lbound, lrect.h, lbound, lrect.h + 100.0, 15.0, WHITE);
-        draw_line(rbound, rrect.h, rbound, rrect.h + 100.0, 15.0, WHITE);
-
-        draw_text("Hello, Macroquad!", 20.0, 20.0, 30.0, DARKGRAY);
+        if is_key_down(KeyCode::W) && lrect.y > top {
+            lrect.y -= 4.0;
+        }
+        if is_key_down(KeyCode::S) && lrect.y < bottom -lrect.h {
+            lrect.y += 4.0;
+        }
+        if is_key_down(KeyCode::Up) && rrect.y > top {
+            rrect.y -= 4.0;
+        }
+        if is_key_down(KeyCode::Down) && rrect.y < bottom -lrect.h{
+            rrect.y += 4.0;
+        }
+        if ball.overlaps_rect(&lrect) {
+            direction.0 = -1.0;
+        }
+        if ball.overlaps_rect(&rrect) {
+            direction.0 = 1.0;
+        }
+        draw_line(lbound, lrect.y, lbound, lrect.y + 100.0, 5.0, WHITE);
+        draw_line(rbound, rrect.y, rbound, rrect.y + 100.0, 5.0, WHITE);
+        draw_circle(ball.x, ball.y, ball.r, WHITE);
+        draw_text("Hello, Github!", 20.0, 20.0, 30.0, DARKGRAY);
 
         next_frame().await
-    }
+    };
+    println!("{out}");
 }
