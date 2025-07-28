897281a600ca00e50b2172dbedf652744f250f84
Eduard <edp475@student.bham.ac.uk>
2025-07-28 18:27:31 +0100

    made it into an actual game with score, and resets on loss

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ async fn main() {
     let rbound = 600.0;
     let top = 100.0;
     let bottom = 400.0;
+    let mut score = (0,0);
     let mut lrect = Rect::new(lbound, top, 1.0, 100.0);
     let mut rrect = Rect::new(rbound, top, 1.0, 100.0);
     let mut ball = Circle::new((lbound + rbound) / 2.0, top, 12.0);
     let mut direction = (1.0, 1.0);
     let mut ball_speed = 1.0;
-    let out = loop {
+    loop {
         clear_background(BLACK);
         ball.x -= direction.0 * ball_speed;
         ball.y -= direction.1 * ball_speed;
@@ -23,10 +24,14 @@ async fn main() {
             direction.1 = 1.0;
         }
         if ball.x < lbound {
-            break "right wins";
+            ball.move_to(Vec2 { x: (lbound+rbound)/2.0, y: top });
+            score.1 +=1;
+            ball_speed = 1.0;
         }
         if ball.x > rbound {
-            break "left wins";
+            ball.move_to(Vec2 { x: (lbound+rbound)/2.0, y: top });
+            score.0 +=1;
+            ball_speed = 1.0;
         }
         if is_key_down(KeyCode::W) && lrect.y > top {
             lrect.y -= 4.0;
@@ -48,10 +53,11 @@ async fn main() {
         }
         draw_line(lbound, lrect.y, lbound, lrect.y + 100.0, 5.0, WHITE);
         draw_line(rbound, rrect.y, rbound, rrect.y + 100.0, 5.0, WHITE);
+        draw_line(lbound, top-12.0, rbound, top-12.0, 5.0, WHITE);
+        draw_line(lbound, bottom+12.0, rbound, bottom+12.0, 5.0, WHITE);
         draw_circle(ball.x, ball.y, ball.r, WHITE);
-        draw_text("Hello, Github!", 20.0, 20.0, 30.0, DARKGRAY);
+        draw_text(&format!("{}  {}",score.0,score.1), 250.0, 300.0, 100.0, DARKGRAY);
 
         next_frame().await
-    };
-    println!("{out}");
+    }
 }
